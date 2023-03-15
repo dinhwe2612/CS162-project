34ada3c4cf382bc3cfe49d8c8f6dbc75249d62d8
Shibainu13 <shibainu.auth@gmail.com>
2023-03-15 18:43:42 +0700

    nothing changed

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
     const int WindowWidth = 1280;
     const int WindowHeight = 720;
 
-    InitWindow(WindowWidth, WindowHeight, "Fuck");
+    InitWindow(WindowWidth, WindowHeight, "Nice");
 
     SetTargetFPS(60);
 
@@ -14,9 +14,15 @@ int main()
         BeginDrawing();
             ClearBackground(RAYWHITE);
 
+            static float rotation = 0.0f;
+            rotation += 0.5f;
+
             DrawRectangle(100, 100, 100 , 300, RED);
             DrawRectangleV((Vector2){300, 200}, (Vector2){100, 30}, BLUE);
-            DrawRectangleRec((Rectangle){200, 200, 100, 100}, GREEN);
+            // DrawRectangleRec((Rectangle){200, 200, 100, 100}, GREEN);
+            DrawRectanglePro((Rectangle){200, 200, 100, 100}, (Vector2){50, 50}, rotation, GREEN);
+
+            DrawCircle(400, 300, 64, ORANGE);
 
         EndDrawing();
     }
