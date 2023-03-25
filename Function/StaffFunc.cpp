d007854e2472374ef8f29781e5e25c669ea1721d
Mmyxxan <127069162+Mmyxxan@users.noreply.github.com>
2023-03-25 15:12:22 +0700

    250323

--- Function/StaffFunc.cpp
+++ Function/StaffFunc.cpp
@@ -29,13 +29,22 @@ void Creat_A_Semester()
     else
         cout << "Semester Created!"<<'\n';
     string date;
+    address += "/Semester_Info.txt";
     cout << "Select the date the semester starts in term of ddmmyy: ";
     cin >> date;
+    out.open(address.c_str());
+    out << schoolyear << '\n';
+    out << semester << '\n';
+    out << date << '\n';
     //out.open() append mode open file in which folder
     cout<<'\n';
     cout << "Select the date the semester ends in term of ddmmyy: ";
     cin >> date;
+    out << date << '\n';
     cout<<'\n';
+    out.close();
+    //return the address to the below function
+    return;
     //out.open() append mode open file in which folder
     // data in text file, create folder, modify text file of students
     // create semester* semester = new Semester.
@@ -56,3 +65,100 @@ void Creat_A_Semester()
  //   Creat_A_Semester();
  //    return 0;
  //}
+void AddCourse(string address)
+{
+    ifstream in;
+    ofstream out;
+    string txtaddress = address + "/Semester_Info.txt";
+    out.open(txtaddress.c_str(), fstream::app);
+    cout << "Please input your course id: ";
+    string courseid;
+    cin >> courseid;
+    cout << '\n';
+    out << courseid << '\n';
+    out.close();
+    string courseaddress = address + "/" + courseid;
+    if(mkdir(courseaddress.c_str()) == -1)
+        cerr << " Error : " << strerror(errno) << '\n';
+    else
+        cout << "Course Created!"<<'\n';
+    cout << "Now continue to complete the course information!";
+    cout << '\n';
+    //use data abstraction
+    txtaddress = courseaddress + "/Course_Info.txt";
+    Course_Information(txtaddress);
+    return;
+}
+
+
+void Course_Information(string txtaddress)
+{
+    ofstream out;
+    out.open(txtaddress.c_str());
+    string str;
+    cout << "What is the course name?" << '\n';
+    cin >> str;
+    cout << '\n';
+    out << str << '\n';
+    cout << "What is the class's name, who would attend this course?" << '\n';
+    cin >> str;
+    cout << '\n';
+    out << str << '\n';
+    cout << "What is the teacher's name, who's in charge of this course?" << '\n';
+    cin >> str;
+    cout << '\n';
+    out << str << '\n';
+    cout << "What is the number of credits for this course?" << '\n';
+    cin >> str;
+    cout << '\n';
+    out << str << '\n';
+    cout << "What is the maximum number of students for the course, default is 50?" << '\n';
+    cin >> str;
+    cout << '\n';
+    out << str << '\n';
+    cout << "What is the day of the week for this course, typing in MON TUE WED THU FRI SAT?" << '\n';
+    // enum?
+    cin >> str;
+    cout << '\n';
+    out << str << '\n';
+    cout << "What is the session in the day, typing in S1 (7:30), S2 (9:30), S3 (13:30) or S4 (15:30)?" << '\n';
+    cin >> str;
+    cout << '\n';
+    out << str << '\n';
+    out.close();
+    return;
+}
+
+
+void View_ListofCourses(string address)
+{
+    ifstream in;
+    string txtaddress = address + "/Semester_Info.txt";
+    in.open(txtaddress.c_str());
+    int i = 0;
+    string str;
+    while (in >> str)
+    {
+        i++;
+        if (i > 4)
+        {
+            ifstream another_in;
+            string another_str;
+            another_str = address + str + "/Course_Info.txt";
+            another_in.open(another_str.c_str());
+            another_in >> another_str;
+            cout << "Course " << i-4 << " : " << str << " " << another_str << '\n';
+            another_in.close();
+        }
+    }
+    in.close();
+    return;
+}
+
+
+void Update_CourseInformation(string address)
+{}
+
+void AddStudenttoCourse() {}
+void RemoveStudentfromCourse() {}
+// bout to done.
--- Header/StaffFunc.h
+++ Header/StaffFunc.h
@@ -5,3 +5,14 @@
 using namespace std;
 
 void Creat_A_Semester();
+
+void AddCourse(string address);
+void Course_Information(string txtaddress);
+void View_ListofCourses(string address);
+void Update_CourseInformation(string address);
+
+void AddStudenttoCourse();
+void RemoveStudentfromCourse();
+
+void Display();
+void MultiChoices();
