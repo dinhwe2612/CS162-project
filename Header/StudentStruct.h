05d41e7cbe545164ac8650074d31945c9e00a7c2
idirius <122882721+idirius@users.noreply.github.com>
2023-03-27 11:17:17 +0700

    clean up and modify gitignore to ignore data files

--- Header/StudentStruct.h
+++ Header/StudentStruct.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream> 
 #include <string.h>
-#include<direct.h>
+#include <direct.h>
 // IF YOU WANT TO UPDATE ONE SPECIFIC FILE, 2 FUNCTIONS IN StaffFunc.h  MAY HELP.
 // FOR SPECIFIC, THEY TAKE IN THE INFORMATION AS A BLOCK OF STRING THAT
 // YOU'RE ABLE TO UPDATE, AND THEN OVERWRITE THE OLD FILE WITH NEW INFORMATION.
@@ -11,7 +11,6 @@ using namespace std;
 struct Semester {
     string *nameOfCourses;
     float **scroreOfCourses;
-    // course id consists of characters?
     string* start_date;
     string* end_date;
 };
@@ -25,9 +24,7 @@ struct Student {
     string studentID;
     string socialID;
     string DOB;// date of birth
-    int Gender;// 0 1 2 : Male Female Other
+    int gender;// 0 1 2 : Male Female Other
     string Class;
     SchoolYear *listOfSchoolYears;
 };
-
-//why not struct Course?
