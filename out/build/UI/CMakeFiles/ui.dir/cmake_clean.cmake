c049a25b569314e2d6017c8e4b419e9f7c8edffd
Shibainu13 <shibainu.auth@gmail.com>
2023-03-28 21:58:01 +0700

    reset main branch local

---
+++ out/build/UI/CMakeFiles/ui.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ui.dir/Button.cpp.o"
+  "CMakeFiles/ui.dir/Button.cpp.o.d"
+  "CMakeFiles/ui.dir/InputBar.cpp.o"
+  "CMakeFiles/ui.dir/InputBar.cpp.o.d"
+  "CMakeFiles/ui.dir/LoginUI.cpp.o"
+  "CMakeFiles/ui.dir/LoginUI.cpp.o.d"
+  "CMakeFiles/ui.dir/StaffUI.cpp.o"
+  "CMakeFiles/ui.dir/StaffUI.cpp.o.d"
+  "libui.a"
+  "libui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/external/raylib/raylib/external/glfw/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/win/Documents/Stupid subjects/CS162/CS162-project/external/raylib/src/external/glfw/src
+# Build directory: /Users/win/Documents/Stupid subjects/CS162/CS162-project/out/build/external/raylib/raylib/external/glfw/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
