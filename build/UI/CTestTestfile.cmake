5473c93947d8531b345ddea65741fd709d149775
idirius <122882721+idirius@users.noreply.github.com>
2023-03-28 17:22:17 +0700

    update repo

---
+++ build/UI/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/User/university/repos/CS162-project/UI
+# Build directory: C:/Users/User/university/repos/CS162-project/build/UI
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
