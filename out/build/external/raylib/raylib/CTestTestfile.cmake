7af40f2dd3ca7827813fa362d22db76ec9afc39d
dinhwe2612 <116061849+dinhwe2612@users.noreply.github.com>
2023-03-31 01:54:55 +0700

    Delete out/build directory

--- out/build/external/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/win/Documents/Stupid subjects/CS162/CS162-project/external/raylib/src
-# Build directory: /Users/win/Documents/Stupid subjects/CS162/CS162-project/out/build/external/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
--- out/build/external/raylib/raylib/external/glfw/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/win/Documents/Stupid subjects/CS162/CS162-project/external/raylib/src/external/glfw/src
-# Build directory: /Users/win/Documents/Stupid subjects/CS162/CS162-project/out/build/external/raylib/raylib/external/glfw/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
