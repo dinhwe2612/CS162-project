5223e3dfa9e814a4c4b413e87b32d3a4aab81dd1
Shibainu13 <shibainu.auth@gmail.com>
2023-04-07 11:49:29 +0700

    template for view, add and drop file

--- UI/StaffUI_Class.hpp
+++ UI/StaffUI_Class.hpp
@@ -20,19 +20,26 @@ struct Class
 
     Button addClass;
     Button close;
+    Button view, drop, add, back;
 
     InputBar inputClass;
 
     std::array<std::string, 100> ListOfClasses = {"22TT1", "22TT2", "21TT1", "21TT2", "20TT1", "20TT2", "19TT1", "19TT2", "18TT1", "18TT2"}; 
     int listSize = 10;
-    
+
+    std::string classDir;
+
+
     void Construct(int windowWidth, int windowHeight);
     void Deconstruct();
+
+    std::string LoadDroppedFile();
 
     void Draw();
         void DrawBackground();
         void DrawCreateClass();
         void DrawClassList();
+        void DrawViewClass();
 };
 
 #endif
